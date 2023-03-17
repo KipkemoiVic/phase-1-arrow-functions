3d4938c8339430dd3acf2ad36fc5d8f1577e9260
VICTOR MITINGI <mitingi.victor@gmail.com>
2023-03-17 20:16:34 +0300

    Completed assignment

---
+++ index.js
@@ -0,0 +1,17 @@
+// index.js
+
+// Define the divide function
+function divide() {
+    return 2000 / 100;
+  }
+  
+  // Define the square function using an arrow function
+  const square = (x) => {
+    return x * x;
+  }
+  
+  // Define the add function using an arrow function
+  const add = (a, b) => {
+    return a + b;
+  }
+  
